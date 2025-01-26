cfc3c20b147dc083c019944017433a1b3fcbd286
Leandro Lomonaco <leandro.lomonaco@gmail.com>
2025-01-26 00:37:48 -0300

    day 2

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+using Unity.Netcode;
+using System.Linq;
+using JetBrains.Annotations;
+
+public class GameManager : NetworkBehaviour {
+  public GameObject[] spawnPoints;
+  public GameObject[] players = new GameObject[0];
+
+
+
+  public void registerPlayer(GameObject player) {
+    Debug.Log("Player registered");
+    /*players = players.Append(player).ToArray();
+    player.transform.position = spawnPoints[players.Length].transform.position;*/
+
+  }
+
+  public void Update() {
+    NetworkManager.Singleton.OnConnectionEvent += (sourceId, data) => {
+      ulong clientId = data.ClientId;
+      if (data.EventType == ConnectionEvent.ClientConnected) {
+        Debug.Log("Client connected: " + clientId);
+        NetworkObject playerObject = NetworkManager.Singleton.ConnectedClients[clientId].PlayerObject;
+        int playerCount = NetworkManager.Singleton.ConnectedClientsList.Count();
+        playerObject.transform.position = spawnPoints[playerCount - 1].transform.position;
+        playerObject.gameObject.name = "Player " + playerCount;
+      }
+    };
+  }
+
+}
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,31 +1,77 @@
 using Unity.Netcode;
 using Unity.VisualScripting;
 using UnityEngine;
-using static UnityEngine.Rendering.DebugUI;
+using UnityEngine.UI;
 
-public class Player : NetworkBehaviour
-{
+public class Player : NetworkBehaviour {
 
-    [SerializeField] GameObject bubble;
+  [SerializeField] GameObject bubble;
+  public GameManager gameManager;
+  public Button blowBtn;
+  public RectTransform canvasRT;
+  public RectTransform blowButtonRT;
 
-    public NetworkVariable<int> bubbleSize = new(1);
-    public override void OnNetworkSpawn() {
-        Debug.Log("Connected");
+
+  public NetworkVariable<int> bubbleSize = new(1);
+  public override void OnNetworkSpawn() {
+    gameManager.registerPlayer(gameObject);
+
+  }
+
+  public void Awake() {
+    gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
+    blowBtn = GameObject.Find("BlowButton").GetComponent<Button>();
+    canvasRT = GameObject.Find("GameCanvas").GetComponent<RectTransform>();
+    blowButtonRT = blowBtn.GetComponent<RectTransform>();
+    blowBtn.onClick.AddListener(() => {
+      if (IsOwner) {
+        SubmitBlowRpc();
+        MoveButtonToRandomPosition();
+      }
+    });
+  }
+  public void Update() {
+    if (Input.GetKeyDown(KeyCode.Space)) {
+      if (IsOwner) {
+        Debug.Log(bubbleSize.Value);
+        SubmitBlowRpc();
+      }
     }
+    float newSize = 1f + (bubbleSize.Value / 25f);
+    bubble.transform.localScale = new Vector3(newSize, newSize, newSize);
+  }
 
-    public void Update() {
-        if (Input.GetKeyDown(KeyCode.Space)) {
-            Debug.Log("ahi va");
-            SubmitBlowRpc();
-        }
+  [Rpc(SendTo.Server)]
+  void SubmitBlowRpc(RpcParams rpcParams = default) {
+    bubbleSize.Value += 1;
+    float newSize = 1f + (bubbleSize.Value / 25f);
+    bubble.transform.localPosition = bubble.transform.localPosition + new Vector3(0, 0.07f, 0);
+    bubble.transform.localScale = new Vector3(newSize, newSize, newSize);
+  }
 
-        bubble.transform.localScale = new Vector3(bubbleSize.Value, bubbleSize.Value, bubbleSize.Value);
-    }
+  public void MoveButtonToRandomPosition() {
+    // Get the dimensions of the button
+    float btnWidth = blowBtn.GetComponent<RectTransform>().rect.width;
+    float btnHeight = blowBtn.GetComponent<RectTransform>().rect.width;
 
-    [Rpc(SendTo.Server)]
-    void SubmitBlowRpc(RpcParams rpcParams = default) {
-        bubbleSize.Value += 1;
-        bubble.transform.localScale = new Vector3(bubbleSize.Value, bubbleSize.Value, bubbleSize.Value);
-    }
+    // Get the dimensions of the canvas
+    float canvasWidth = canvasRT.rect.width;
+    float canvasHeight = canvasRT.rect.height;
+
+    // Calculate the bounds for the button
+    float xMin = (canvasWidth * -0.5f) + btnWidth / 2;
+    float xMax = (canvasWidth * 0.5f) - btnWidth / 2;
+    float yMin = (canvasHeight * -0.5f) + btnHeight / 2;
+    float yMax = (canvasHeight * 0.5f) - btnHeight / 2;
+
+    // Generate random position within bounds
+    Debug.Log("xMin: " + xMin + " xMax: " + xMax + " yMin: " + yMin + " yMax: " + yMax);
+    float randomX = Random.Range(xMin, xMax);
+    float randomY = Random.Range(yMin, yMax);
+
+    // Set the button position
+    blowButtonRT.localPosition = new Vector3(randomX, randomY, 0);
+
+  }
 }
 
